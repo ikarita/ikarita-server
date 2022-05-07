ea355641669b4371cfc13f713b0cc0e0572333c4
kabinja <renaudrwemalika@gmail.com>
2022-05-07 11:56:00 +0200

    Initial commit

---
+++ src/main/java/com/github/ikarita/server/IkaritaServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.ikarita.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkaritaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkaritaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ikarita/server/IkaritaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ikarita.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkaritaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
