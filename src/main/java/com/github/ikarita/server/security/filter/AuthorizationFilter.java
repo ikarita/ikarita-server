ea6cbcd40a30fef52e053fa65f6b988e518ded3e
kabinja <renaudrwemalika@gmail.com>
2022-05-09 20:45:37 +0200

    rework on security, but the authority based system is not there yet.

--- src/main/java/com/github/ikarita/server/security/filter/AuthorizationFilter.java
+++ src/main/java/com/github/ikarita/server/security/filter/AuthorizationFilter.java
@@ -23,7 +23,7 @@ public class AuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (requiresValidation(request)) {
-            jwtUtils.validateJWT(request, response, filterChain);
+            jwtUtils.validateJWT(request, response);
         }
 
         filterChain.doFilter(request, response);
