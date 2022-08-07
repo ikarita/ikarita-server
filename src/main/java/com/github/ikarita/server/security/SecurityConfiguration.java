401f65af06599e982bb8db81da20e71c981ef855
kabinja <renaudrwemalika@gmail.com>
2022-08-07 07:26:13 +0200

    configure security

--- src/main/java/com/github/ikarita/server/security/SecurityConfiguration.java
+++ src/main/java/com/github/ikarita/server/security/SecurityConfiguration.java
@@ -1,27 +1,65 @@
 package com.github.ikarita.server.security;
 
-import com.c4_soft.springaddons.security.oauth2.spring.C4MethodSecurityExpressionHandler;
-import com.c4_soft.springaddons.security.oauth2.spring.C4MethodSecurityExpressionRoot;
-import lombok.RequiredArgsConstructor;
+import com.google.common.net.HttpHeaders;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+import java.util.List;
+
+
 @EnableWebSecurity
-@RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     @Bean
-    public MethodSecurityExpressionHandler methodSecurityExpressionHandler() {
-        return new C4MethodSecurityExpressionHandler(IkaritaMethodSecurityExpressionRoot::new);
+    public SecurityFilterChain filterChain(HttpSecurity http, Converter<Jwt, ? extends AbstractAuthenticationToken> authenticationConverter, ServerProperties serverProperties)
+            throws Exception {
+
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(authenticationConverter);
+        http.csrf().disable();
+        http.anonymous();
+        http.cors().configurationSource(corsConfigurationSource());
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+            response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Restricted Content\"");
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        });
+
+        if (serverProperties.getSsl() != null && serverProperties.getSsl().isEnabled()) {
+            http.requiresChannel().anyRequest().requiresSecure();
+        } else {
+            http.requiresChannel().anyRequest().requiresInsecure();
+        }
+
+        http.authorizeRequests()
+                .antMatchers("/actuator/health/readiness", "/actuator/health/liveness", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
     }
 
-    static final class IkaritaMethodSecurityExpressionRoot extends C4MethodSecurityExpressionRoot {
-        public boolean hasCommunityAuthority(Long communityId, String role){
-            return false;
-        }
+    private CorsConfigurationSource corsConfigurationSource() {
+        final var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+
+        final var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api.v1/**", configuration);
+
+        return source;
     }
 }
--- src/test/java/com/github/ikarita/server/api/user/DataUserControllerTest.java
+++ src/test/java/com/github/ikarita/server/api/user/DataUserControllerTest.java
@@ -6,14 +6,11 @@ import com.c4_soft.springaddons.security.oauth2.test.mockmvc.AutoConfigureSecuri
 import com.github.ikarita.server.security.SecurityConfiguration;
 import com.github.ikarita.server.service.user.DataUserService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Collections;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
